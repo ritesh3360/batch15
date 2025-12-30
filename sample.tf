ef88b9ea3d23e7758767839b8b51eca3cb88547d
ritesh <riteshkadam914@gmail.com>
2025-12-30 10:02:49 +0530

    initial instance created

--- sample.tf
+++ sample.tf
@@ -4,8 +4,8 @@ provider "aws" {
 resource "aws_instance" "myinstance" {
     ami = "ami-0ecb62995f68bb549"
     instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-0d378b7062c573f2e"]
-    key_name = "id_rsp.pem"
+    vpc_security_group_ids = ["sg-0bb8172ca33f18712"]
+    key_name = "id_rsp"
     availability_zone = "us-east-1"
     tags = {
       name ="myinstance"
