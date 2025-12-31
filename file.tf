bc2ea0689cc5d3fc92958289a51aec4ec75816c8
ritesh <riteshkadam914@gmail.com>
2025-12-31 13:04:58 +0530

    initial instance creation

--- file.tf
+++ file.tf
@@ -2,16 +2,16 @@ provider "aws" {
     region = "us-east-1" 
 }
 resource "aws_instance" "myinstance" {
-    ami = "ami-0ecb62995f68bb549"
+    ami = "ami-0c398cb65a93047f2"
     instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-0bb8172ca33f18712"]
+    vpc_security_group_ids = ["sg-0ebaff8e72132920e"]
     key_name = "id_rsp"
-    availability_zone = "us-east-1"
+    availability_zone = "us-east-1a"
     tags = {
-      name ="myinstance"
+      Name ="myinstance"
     }
   
 }
-resource "aws_security_group" "testing" {
-  
-}
+
+
+
