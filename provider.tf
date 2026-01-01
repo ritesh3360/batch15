1542fdc3a205c867240c6a9b6cfdda4b58e4fcc4
ritesh3360 <riteshkadam914@gmail.com>
2026-01-01 19:06:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,1 @@
-variable "ami_id" {
-    default = "ami-0c398cb65a93047f2"
-}
-variable "instance_type" {
-    default = "t3.micro"
-}
-variable "key_name" {
-    default = "id_ritesh"
-}
-variable "vpc_id" {
-    default = ""
-  
-}
+
