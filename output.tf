1b85c2c0d2e6c861ef44595b4e56dd32499fcd13
ritesh3360 <riteshkadam914@gmail.com>
2026-01-01 22:11:00 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,7 @@
 output "instance_public_ip" {
-  value = aws_instance.myinstance.public_ip_address
+  value = aws_instance.myinstance.public_ip
 }
+
 output "instance_id" {
   value = aws_instance.myinstance.id
 }
