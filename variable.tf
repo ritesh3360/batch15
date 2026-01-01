38038f4d9ed3ede0f6d44141afb44878fcb2a832
ritesh <riteshkadam914@gmail.com>
2026-01-01 19:10:04 +0530

    seperate files created

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+  default = "ami-0c398cb65a93047f2"
+}
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "key_name" {
+  default = "id_dsp."
+}
+ variable "vpc_id" {
+   default = "vpc-00d7f48dd05818b0b"
+ }
