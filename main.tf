108530a6d1d9f5500b2fe7531c8701d96287e340
ritesh3360 <riteshkadam914@gmail.com>
2026-01-01 21:53:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,35 @@
 locals {
-   instance_name = "my-instance"
- }
- terraform {
-   backend "s3" {
-     bucket = "batch3360"
-     region = "us-east-1"
-     key = "tfstatefile"
-   }
- }
-data "aws_security_group" "mysg0"{
+  instance_name = "my-instance"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "batch3360"
+    region = "us-east-1"
+    key    = "tfstatefile"
+  }
+}
+
+data "aws_security_group" "mysg0" {
   filter {
-    name = "vpc-id"
+    name   = "vpc-id"
     values = [var.vpc_id]
   }
+
   filter {
-    name = "group-name"
+    name   = "group-name"
     values = ["mysg0"]
   }
- }
- resource "aws_instance" "myinstance" {
-   ami = var.ami_id
-     instance_type = var.instance_type
-     vpc_security_group_ids = [data.aws_security_group.mysg0]
-     key_name = var.key_name
-     availability_zone = "us-east-1a"
-     tags = {
-       Name = local.instance_name
-     }
- }
+}
+
+resource "aws_instance" "myinstance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.mysg0.id]
+  key_name               = var.key_name
+  availability_zone      = "us-east-1a"
+
+  tags = {
+    Name = local.instance_name
+  }
+}
