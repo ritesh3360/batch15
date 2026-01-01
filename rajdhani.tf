8367e206c6a8e51e49f84c0a50a48c30cb5cce68
ritesh3360 <riteshkadam914@gmail.com>
2026-01-01 15:26:52 +0530

    Update rajdhani.tf

--- rajdhani.tf
+++ rajdhani.tf
@@ -23,16 +23,17 @@ variable "ami_id" {
   default = "ami-0c398cb65a93047f2"
 }
 variable "instance_type" {
-default = "t3.micro"
+  default = "t3.micro"
 }
 variable "key_name" {
-  default = "id_dsp.pem"
+  default = "id_dsp."
 }
-output "instance_id" {
-  value = aws_instance.myinstance.ip
+output "instance_public_id" {
+  value = "aws_instance.myinstance.ip"
 }
 output "instance_id" {
   value = aws_instance.myinstance.id
 }
+
 
 
