05bddb501f216a195b6694f0b622c4c8f5c7b44f
ritesh3360 <riteshkadam914@gmail.com>
2026-01-01 12:38:02 +0530

    Update rajdhani.tf

--- rajdhani.tf
+++ rajdhani.tf
@@ -9,14 +9,18 @@ provider "aws" {
     region = "us-east-1" 
 }
 resource "aws_instance" "myinstance" {
-    ami = "ami-0c398cb65a93047f2"
-    instance_type = "t3.micro"
+    ami = var.ami_id
+    instance_type = var.instance_type
     vpc_security_group_ids = ["sg-00d5bac945ac6b509"]
-    key_name = "id_dsp"
+    key_name = var.key_name
     availability_zone = "us-east-1a"
     tags = {
       Name ="myinstance"
     }
-  
 }
+
+variable "ami_id" {
+  default = "ami-0c398cb65a93047f2"
+}
+variable "instance_type" {
 
